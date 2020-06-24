11e7e741c236a7bdbaa88a1d6c88ce3ace073520
Eckhart Koeppen <eck@40hz.org>
2020-06-24 19:38:54 +0300

    Convert line endings

--- Drivers/TSerialChipEinstein.cpp
+++ Drivers/TSerialChipEinstein.cpp
@@ -1,1 +1,35 @@
-// ==============================// File:			TSerialChipEinstein.cp// Project:			Einstein//// Copyright 2020 by Eckhart Koeppen (eck@40hz.org)//// This program is free software; you can redistribute it and/or modify// it under the terms of the GNU General Public License as published by// the Free Software Foundation; either version 2 of the License, or// (at your option) any later version.//// This program is distributed in the hope that it will be useful,// but WITHOUT ANY WARRANTY; without even the implied warranty of// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the// GNU General Public License for more details.//// You should have received a copy of the GNU General Public License along// with this program; if not, write to the Free Software Foundation, Inc.,// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.// ==============================// $Id$// ==============================#include "TSerialChipEinstein.impl.h"#include <OptionArray.h>#include <HALOptions.h>#include <CommErrors.h>PROTOCOL_IMPL_SOURCE_MACRO(TSerialChipEinstein)	// Magic stuff, do not touch// -------------------------------------------------------------------------- //// Constantes// -------------------------------------------------------------------------- //// -------------------------------------------------------------------------- ////  * New( void )// -------------------------------------------------------------------------- //TSerialChip*TSerialChipEinstein::New( void ){	return this;}// -------------------------------------------------------------------------- ////  * Delete( void )// -------------------------------------------------------------------------- //voidTSerialChipEinstein::Delete( void ){}// -------------------------------------------------------------------------- ////  * InitByOption( TOption* )// -------------------------------------------------------------------------- //NewtonErrTSerialChipEinstein::InitByOption( TOption* inOption ){	mLocationID = ((THMOSerialEinsteinHardware*) inOption)->fLocationID;	return noErr;}// =============================== //// //GO.SYSIN DD *, DOODAH, DOODAH //// =============================== //
+// ==============================
+// File:			TSerialChipEinstein.cp
+// Project:			Einstein
+//
+// Copyright 2020 by Eckhart Koeppen (eck@40hz.org)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// ==============================
+// $Id$
+// ==============================
+
+#include "TSerialChipEinstein.impl.h"
+#include <OptionArray.h>
+#include <HALOptions.h>
+#include <CommErrors.h>
+
+extern "C" void Reset(void);
+
+PROTOCOL_IMPL_SOURCE_MACRO(TSerialChipEinstein)	// Magic stuff, do not touch
+
+// =============================== //
+// //GO.SYSIN DD *, DOODAH, DOODAH //
+// =============================== //
